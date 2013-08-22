501e509e43a9d3413fe03e265a3a5ab4749f6e3a
Harry <hjwp2@cantab.net>
2013-08-22 20:35:18 +0100

    move a method and tweak it --ch17l029--

--- pythonfile.py
+++ pythonfile.py
@@ -6,15 +6,17 @@ from lists.stuff import things
 
 class Klassey(things.Thing):
 
+    def method2(self, b):
+        # do things
+        return b + 2
+
+class NuKlass(object):
+
     def method1(self):
         # amend method 1
         # it's a litltle longer
         a = 2
-        a = a + 1
+        a = a + 3
         b = a
         return 2
-
-    def method2(self, b):
-        # do things
-        return b + 2
 
